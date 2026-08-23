3bdcbdd9f3840cb6527f995d1ecac0915aa792b4
Luan Santos <luankauan10@gmail.com>
2026-08-22 21:51:55 -0300

    added arvore balanciada vs Z4

--- tree/decisionTree-1.0.py
+++ tree/decisionTree-1.0.py
@@ -104,7 +104,7 @@ param_grid = {
     "criterion": ["entropy", "gini"],
     #"max_depth": [3, 4, 5, 6, 8, None],
     #"max_depth": [5, 10],
-    "max_depth": [5],
+    "max_depth": [10],
     #"max_depth": [10],
     "min_samples_leaf": [1, 5, 10, 20],
     "min_samples_split": [2, 10, 20],
@@ -113,6 +113,7 @@ param_grid = {
 cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
 
 grid = GridSearchCV(
+    #DecisionTreeClassifier(random_state=42, class_weight="balanced"),
     DecisionTreeClassifier(random_state=42),
     param_grid,
     scoring="balanced_accuracy",
